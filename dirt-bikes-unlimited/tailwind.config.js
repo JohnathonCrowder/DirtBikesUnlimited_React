3be547165fbad546adc9de175dee00b553fe2719
Johnathon Crowder <139363360+JohnathonCrowder@users.noreply.github.com>
2024-12-11 13:43:18 -0600

    init

---
+++ dirt-bikes-unlimited/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2c3e50',
+        secondary: '#34495e',
+        accent: '#e67e22',
+        highlight: '#3498db',
+      },
+      fontFamily: {
+        sans: ['Open Sans', 'sans-serif'],
+        heading: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
